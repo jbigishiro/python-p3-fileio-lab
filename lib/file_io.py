d33e798ceb9e25ee481e2f5113f33aacfb441298
Justin Bigishiro <justin.rvd@gmail.com>
2023-08-04 06:58:30 -0500

    finish the lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as file:
+        content=file.write(file_content)
+    return content
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as file:
+        content=file.write(append_content)
+    return content
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as file:
+        content = file.read()
+    return content
